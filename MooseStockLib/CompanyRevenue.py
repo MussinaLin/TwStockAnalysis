139dda7da2fad501e58af293d6312e3a750f7e69
MussinaLin <mussinalin@gmail.com>
2018-11-11 15:12:50 +0800

    CompanyRevenue monthly_report fn done.

---
+++ MooseStockLib/CompanyRevenue.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 23:25:27 2018
+
+@author: Mussina
+"""
+
+import pandas as pd
+import requests
+from io import StringIO
+import time
+
+def monthly_report(year, month):
+    
+    # 假如是西元，轉成民國
+    if year > 1990:
+        year -= 1911
+    
+    url = 'http://mops.twse.com.tw/nas/t21/sii/t21sc03_'+str(year)+'_'+str(month)+'_0.html'
+    if year <= 98:
+        url = 'http://mops.twse.com.tw/nas/t21/sii/t21sc03_'+str(year)+'_'+str(month)+'.html'
+    
+    # 偽瀏覽器
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    
+    # 下載該年月的網站，並用pandas轉換成 dataframe
+    r = requests.get(url, headers=headers)
+    r.encoding = 'big5'
+    html_df = pd.read_html(StringIO(r.text))
+    
+    # 處理一下資料
+    if html_df[0].shape[0] > 500:
+        df = html_df[0].copy()
+    else:
+        df = pd.concat([df for df in html_df if df.shape[1] <= 11])
+    df = df[list(range(0,10))]
+    column_index = df.index[(df[0] == '公司代號')][0]
+    df.columns = df.iloc[column_index]
+    df['當月營收'] = pd.to_numeric(df['當月營收'], 'coerce')
+    df = df[~df['當月營收'].isnull()]
+    df = df[df['公司代號'] != '合計']
+    
+    # 偽停頓
+    time.sleep(5)
+    return df
--- main.py
+++ main.py
@@ -7,18 +7,30 @@ Created on Sat Nov 10 16:03:29 2018
 import configparser
 import logging
 import logging.config
+import twstock
 
+#self made module
+from MooseStockLib import CompanyRevenue
 # logger setting
 logging.config.fileConfig("./config/logconfig.ini")
 logger = logging.getLogger("root")
 
+logging.info("===== System Start =====")
+
 # config setting
+logging.info("[v] read config")
 config = configparser.ConfigParser()
 config.read('./config/config.ini')
-
+logging.info("[^] read config")
 
 temp = config.get('Stock', 'ToBeAnalysisStock')
 ToBeAnalysisStock = temp.split(",")
-print("ToBeAnalysisStock:",ToBeAnalysisStock)
+logging.info("ToBeAnalysisStock:%s",ToBeAnalysisStock)
 
-logging.info('This is info message')
+
+#monthRevenue = CompanyRevenue.monthly_report(2018,10)
+#print(monthRevenue)
+
+
+
+logging.info("===== System End =====")
