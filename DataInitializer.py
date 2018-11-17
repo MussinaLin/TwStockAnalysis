03d2b53b854ad65286c385b9757d0676d6fd82ac
MussinaLin <mussinalin@gmail.com>
2018-11-17 21:28:23 +0800

    try to initial all companys data in CompanyData.
    try to write log in self made library

---
+++ DataInitializer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 20:52:52 2018
+
+@author: Mussina
+"""
+
+class StockDataInitializer():
+    def __init__(self):
+        self.data = []
+    
+    """
+    iniial all companys data in CompanyData folder
+    """
+    def iniCompanyData(self, companys):
+        print(companys)
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import pandas as pd
 #self made module
 from MooseStockLib import CompanyRevenue
 from MooseStockLib.DataRetriever import HisDataRetriever
+from DataInitializer import StockDataInitializer
 # logger setting
 logging.config.fileConfig('./config/logconfig.ini')
 logger = logging.getLogger('root')
@@ -28,7 +29,8 @@ temp = config.get('Stock', 'ToBeAnalysisStock')
 ToBeAnalysisStock = temp.split(',')
 logging.info('ToBeAnalysisStock:%s',ToBeAnalysisStock)
 
-
+ini = StockDataInitializer()
+ini.iniCompanyData(ToBeAnalysisStock)
 #monthRevenue = CompanyRevenue.monthly_report(2018,10)
 #print(monthRevenue)
 #stock = twstock.Stock('2330')
@@ -37,8 +39,12 @@ logging.info('ToBeAnalysisStock:%s',ToBeAnalysisStock)
 tt = pd.read_excel('./CompanyData/2330.xlsx')
 print(tt)
 
-retriever = HisDataRetriever(logger)
-stock = retriever.fetchLast31('2330')
-print(stock.price)
+retriever = HisDataRetriever()
+#stock = retriever.fetchLast31('2330')
+#print(stock.price)
 
+#%%
+print(stock.date[0])
+print(stock.date[0].strftime('%Y-%m-%d'))
+#%%
 logging.info('===== System End =====')
