bd9da8be2c9cdc5ec88455636cb62281053b6585
MussinaLin <mussinalin@gmail.com>
2019-01-11 17:54:02 +0800

    calculate DM+ done.

--- MooseStockLib/TechAnalysis/DirectionalMovementIndex.py
+++ MooseStockLib/TechAnalysis/DirectionalMovementIndex.py
@@ -23,9 +23,63 @@ class DMI():
             self.logger.info("file not exist...please create [%s] file first.",filePath)
             return
         
-        excel_writer = pd.ExcelWriter(filePath)
+        #excel_writer = pd.ExcelWriter(filePath)
         df_DMI = pd.read_excel(filePath)
+        
         #df_DMI = df_DMI.dropna()
         
-        print(df_company)
+        self._createDMI(df_company, df_DMI)
+        
+        #excel_writer.close()
         self.logger.info('[^] create DMI')
+        
+    def _createDMI(self, df_company, df_DMI):
+        if df_DMI.last_valid_index() == None:
+            dmi_idx = 0
+        else:
+            dmi_idx = df_DMI.last_valid_index() + 1
+        
+        row_num = len(df_company.index)
+        for i in range(row_num):
+            if i == 0:
+                DM_plus = 0
+            else:
+                DM_plus = df_company.iloc[i]['最高價'] - df_company.iloc[i-1]['最高價']
+            
+            if DM_plus < 0:
+                DM_plus = 0
+                
+            df_DMI.loc[dmi_idx] = [df_company.iloc[i]['日期'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最低價'],
+                                   df_company.iloc[i]['收盤價'],
+                                   DM_plus,
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價'],
+                                   df_company.iloc[i]['最高價']                                
+                                  ]
+            #df_DMI.iloc[dmi_idx]['最低價'] = df_company.iloc[i]['最低價']
+            dmi_idx += 1         
+            # --- get start index --- #
+            
+        print(df_DMI)
+        #--- set data to dataframe ---#
+            #df_company['最高價']
+    
+    
+    
+    
+    
+    
+    
+    
