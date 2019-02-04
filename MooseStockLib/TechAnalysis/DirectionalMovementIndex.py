156d27b33ac2e2ef0ed08084a5a5b6ad267add13
Mussina Lin <mussina.lin@nogle.com>
2019-02-04 22:59:13 +0800

    add calculate TR(True Range)

--- MooseStockLib/TechAnalysis/DirectionalMovementIndex.py
+++ MooseStockLib/TechAnalysis/DirectionalMovementIndex.py
@@ -48,10 +48,15 @@ class DMI():
                 DM_minus = 0
                 DM_Plus_Pulan = 0
                 DM_Minus_Pulan = 0
+                Ht_Lt = 0
+                Ht_Ct = 0
+                Lt_Ct = 0
+                TR = 0
             else:
                 DM_plus = self._cal_DM_Plus(df_company.iloc[i]['最高價'], df_company.iloc[i-1]['最高價'])
                 DM_minus = self._cal_DM_Minus(df_company.iloc[i-1]['最低價'], df_company.iloc[i]['最低價'])
                 (DM_Plus_Pulan, DM_Minus_Pulan) = self._cal_DM_Plus_Minus_Pulan(DM_plus, DM_minus)
+                (Ht_Lt, Ht_Ct, Lt_Ct, TR) = self._cal_TR(df_company.iloc[i]['最高價'], df_company.iloc[i]['最低價'],df_company.iloc[i-1]['收盤價'])
                 
             df_DMI.loc[dmi_idx] = [df_company.iloc[i]['日期'],
                                    df_company.iloc[i]['最高價'],
@@ -61,10 +66,10 @@ class DMI():
                                    DM_minus,
                                    DM_Plus_Pulan,
                                    DM_Minus_Pulan,
-                                   df_company.iloc[i]['最高價'],
-                                   df_company.iloc[i]['最高價'],
-                                   df_company.iloc[i]['最高價'],
-                                   df_company.iloc[i]['最高價'],
+                                   Ht_Lt,
+                                   Ht_Ct,
+                                   Lt_Ct,
+                                   TR,
                                    df_company.iloc[i]['最高價'],
                                    df_company.iloc[i]['最高價'],
                                    df_company.iloc[i]['最高價'],
@@ -74,25 +79,35 @@ class DMI():
                                   ]
             #df_DMI.iloc[dmi_idx]['最低價'] = df_company.iloc[i]['最低價']
             dmi_idx += 1        
-            print("DM_plus:{0} DM_minus:{1}".format(DM_plus, DM_minus))
-            print("DM_Plus_Pulan:{0} DM_Minus_Pulan:{1}".format(DM_Plus_Pulan, DM_Minus_Pulan))
+            print("Ht_Lt:{0} Ht_Ct:{1} Lt_Ct:{2} TR:{3}".format(Ht_Lt, Ht_Ct,Lt_Ct,TR))
+            
             # --- get start index --- #
             
         #print(df_DMI)
         #--- set data to dataframe ---#
             #df_company['最高價']
+            
+    '''
+    calaulate DM+
+    '''
     def _cal_DM_Plus(self, highest_price, pre_highest_price):
         DM_plus = highest_price - pre_highest_price
         if DM_plus < 0:
             DM_plus = 0
         return DM_plus
     
+    '''
+    calaulate DM-
+    '''
     def _cal_DM_Minus(self, pre_lowest_price, lowest_price):
         DM_minus = pre_lowest_price - lowest_price
         if DM_minus < 0:
             DM_minus = 0
         return DM_minus
     
+    '''
+    calaulate DM+' & DM-'
+    '''
     def _cal_DM_Plus_Minus_Pulan(self, DM_plus, DM_minus):
         if DM_plus > DM_minus:
             DM_Plus_Pulan = DM_plus
@@ -105,6 +120,17 @@ class DMI():
             DM_Minus_Pulan = 0
         
         return (DM_Plus_Pulan, DM_Minus_Pulan)
+    
+    '''
+    calaulate TR(True Range)
+    '''
+    def _cal_TR(self, highest_price, lowest_price, pre_close_price):
+        Ht_Lt = highest_price - lowest_price
+        Ht_Ct = highest_price - pre_close_price   
+        Lt_Ct = abs(lowest_price - pre_close_price)
+        TR = max(Ht_Lt,Ht_Ct,Lt_Ct)
+        return (Ht_Lt, Ht_Ct, Lt_Ct, TR)
+    
     
     
     
